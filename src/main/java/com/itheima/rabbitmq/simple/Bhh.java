71115acd621781e54b368440144193eee3bf111a
zhangsan <zhangsan@itcast.cn>
2020-10-12 23:44:29 +0800

    Bhh yu Hhh

---
+++ src/main/java/com/itheima/rabbitmq/simple/Bhh.java
@@ -0,0 +1,8 @@
+package com.itheima.rabbitmq.simple;
+
+/**
+ * @author 陈杰华
+ * @date 2020/10/12 - 23:43
+ */
+public class Bhh {
+}
---
+++ src/main/java/com/itheima/rabbitmq/simple/Hhh.java
@@ -0,0 +1,11 @@
+package com.itheima.rabbitmq.simple;
+
+/**
+ * @author 陈杰华
+ * @date 2020/10/12 - 23:42
+ */
+public class Hhh {
+    public static void main(String[] args) {
+        System.out.println("444");
+    }
+}
